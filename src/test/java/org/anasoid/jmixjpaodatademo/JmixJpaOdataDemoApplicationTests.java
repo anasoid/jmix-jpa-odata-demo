0ab2b4fec04e5fb3bcefda793dd8c855d63558b0
anas <oufdou.anas@gmail.com>
2022-05-09 12:28:38 +0100

    init

---
+++ src/test/java/org/anasoid/jmixjpaodatademo/JmixJpaOdataDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.anasoid.jmixjpaodatademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmixJpaOdataDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
