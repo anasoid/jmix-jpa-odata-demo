874cbf383ed143e69a20bd6a4d5f58b8155b0aa6
anas <oufdou.anas@gmail.com>
2022-06-10 16:39:01 +0100

    add boot with eclips link

--- odata/src/main/java/org/anasoid/jmixjpaodatademo/odata/controllers/DefaultController.java
+++ odata/src/main/java/org/anasoid/jmixjpaodatademo/odata/controllers/DefaultController.java
@@ -1,11 +1,15 @@
 package org.anasoid.jmixjpaodatademo.odata.controllers;
 
+import org.anasoid.jmixjpaodatademo.core.entity.catalog.Catalog;
 import org.anasoid.jmixjpaodatademo.odata.repository.CatalogRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.xml.catalog.Catalog;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
 
 @RestController
 public class DefaultController {
@@ -20,7 +24,7 @@ public class DefaultController {
     }
 
     @GetMapping("/catalogs")
-    Iterable<Catalog> catalogs() {
-        return catalogRepository.findAll();
+    List<Catalog> catalogs() {
+        return StreamSupport.stream(catalogRepository.findAll().spliterator(), false).collect(Collectors.toList());
     }
 }
